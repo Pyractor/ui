a45375cb8e937db71a96f2b8fe9b515ed807784c
Max Gonzih <gonzih@gmail.com>
2022-08-15 12:21:48 -0700

    Interactive thing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useCallback } from "react";
 import useWebSocket, { ReadyState } from "react-use-websocket";
+import debounce from "lodash.debounce";
 
 interface Hello {
   session: string;
@@ -24,19 +25,133 @@ interface MessageOut {
   message: Hello | Change;
 }
 
+interface SliderState {
+  id: string;
+  kind: string;
+  visible: boolean;
+  value: number;
+  min: number;
+  max: number;
+}
+
+interface MDState {
+  id: string;
+  kind: string;
+  visible: boolean;
+  md: string;
+}
+
+type ElementState = SliderState | MDState;
+
+type ChangeCb = (id: string, value: any) => void;
+
+interface State {
+  registry: Record<string, ElementState>;
+  order: Array<string>;
+}
+
+const RenderSlider = (props: {
+  id: string;
+  state: SliderState;
+  onChange: ChangeCb;
+}) => {
+  const { state, id, onChange } = props;
+  const [value, setValue] = useState(state.value);
+
+  const change = (v: string) => {
+    const n = parseInt(v, 10);
+    setValue(n);
+  };
+
+  const sync = useCallback(
+    debounce((v: string) => {
+      const n = parseInt(v, 10);
+      onChange(id, n);
+    }, 1000),
+    [id, onChange]
+  );
+
+  return (
+    <div>
+      <input
+        type="range"
+        min={state.min}
+        max={state.max}
+        value={value}
+        onChange={(e) => {
+          change(e.target.value);
+          sync(e.target.value);
+        }}
+      />
+    </div>
+  );
+};
+
+const RenderMD = (props: { id: string; state: MDState }) => {
+  const { state, id } = props;
+
+  return <div>{state.md}</div>;
+};
+
+const RenderElement = (props: {
+  id: string;
+  state: ElementState;
+  onChange: ChangeCb;
+}) => {
+  const { state, id, onChange } = props;
+
+  return (
+    <div>
+      {state.visible && state.kind === "Slider" && (
+        <RenderSlider
+          id={id}
+          state={state as SliderState}
+          onChange={onChange}
+        />
+      )}
+      {state.visible && state.kind === "MD" && (
+        <RenderMD id={id} state={state as MDState} />
+      )}
+    </div>
+  );
+};
+
+const RenderState = (props: { state: State; onChange: ChangeCb }) => {
+  const { state, onChange } = props;
+  return (
+    <div>
+      {state.order.map((id: string) => (
+        <RenderElement
+          id={id}
+          state={state.registry[id]}
+          key={id}
+          onChange={onChange}
+        />
+      ))}
+    </div>
+  );
+};
+
 function App() {
+  const [isDebug, setIsDebug] = useState(process.env.NODE_ENV !== "production");
   const [initialized, setInitialized] = useState(false);
-  const [appState, setAppState] = useState<any>({});
+  const [appState, setAppState] = useState<State>({ registry: {}, order: [] });
   const [socketUrl, setSocketUrl] = useState("ws://localhost:1337");
   const [messageHistory, setMessageHistory] = useState<Array<any>>([]);
   const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl);
 
-  const send = (msg: any) => sendMessage(JSON.stringify(msg));
+  const send = (msg: any) => {
+    console.log(msg);
+    sendMessage(JSON.stringify(msg));
+  };
 
   useEffect(() => {
     if (lastMessage !== null) {
       const json = lastMessage.data;
       const msg = JSON.parse(json) as MessageIn;
+      if (msg.kind === "StateSync") {
+        setAppState(msg.message.state);
+      }
       console.log(msg);
       setMessageHistory((prev) => prev.concat(msg));
     }
@@ -46,7 +161,7 @@ function App() {
     if (ReadyState.OPEN === readyState && !initialized) {
       setInitialized(true);
       const msg = {
-        kind: "hello",
+        kind: "Hello",
         message: { session: "session" },
       } as MessageOut;
 
@@ -57,17 +172,27 @@ function App() {
   const handleClickSendMessage = useCallback(
     () =>
       send({
-        kind: "hello",
+        kind: "Hello",
         message: { session: "session" },
       }),
     []
   );
 
+  const change = (id: string, value: any) => {
+    send({
+      kind: "Change",
+      message: { id, value },
+    });
+  };
+
+  (window as any)["debugme"] = () => setIsDebug((debug) => !debug);
+
   return (
     <div className="App">
       <button onClick={handleClickSendMessage}>CLICK</button>
-      <pre>{JSON.stringify(appState)}</pre>
-      <pre>{JSON.stringify(messageHistory, null, 2)}</pre>
+      <RenderState state={appState} onChange={change} />
+      {isDebug && <pre>{JSON.stringify(appState, null, 2)}</pre>}
+      {isDebug && <pre>{JSON.stringify(messageHistory, null, 2)}</pre>}
     </div>
   );
 }
