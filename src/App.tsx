3bf85768a938b4849df0a3cc5173868388bda245
Max Gonzih <gonzih@gmail.com>
2022-08-15 10:51:45 -0700

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useCallback } from "react";
+import useWebSocket, { ReadyState } from "react-use-websocket";
+
+interface Hello {
+  session: string;
+}
+
+interface Change {
+  id: string;
+  value: any;
+}
+
+interface StateSync {
+  state: any;
+}
+
+interface MessageIn {
+  kind: string;
+  message: StateSync;
+}
+
+interface MessageOut {
+  kind: string;
+  message: Hello | Change;
+}
 
 function App() {
+  const [initialized, setInitialized] = useState(false);
+  const [appState, setAppState] = useState<any>({});
+  const [socketUrl, setSocketUrl] = useState("ws://localhost:1337");
+  const [messageHistory, setMessageHistory] = useState<Array<any>>([]);
+  const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl);
+
+  const send = (msg: any) => sendMessage(JSON.stringify(msg));
+
+  useEffect(() => {
+    if (lastMessage !== null) {
+      const json = lastMessage.data;
+      const msg = JSON.parse(json) as MessageIn;
+      console.log(msg);
+      setMessageHistory((prev) => prev.concat(msg));
+    }
+  }, [lastMessage, setMessageHistory]);
+
+  useEffect(() => {
+    if (ReadyState.OPEN === readyState && !initialized) {
+      setInitialized(true);
+      const msg = {
+        kind: "hello",
+        message: { session: "session" },
+      } as MessageOut;
+
+      send(msg);
+    }
+  }, [initialized, setInitialized, readyState]);
+
+  const handleClickSendMessage = useCallback(
+    () =>
+      send({
+        kind: "hello",
+        message: { session: "session" },
+      }),
+    []
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleClickSendMessage}>CLICK</button>
+      <pre>{JSON.stringify(appState)}</pre>
+      <pre>{JSON.stringify(messageHistory, null, 2)}</pre>
     </div>
   );
 }
